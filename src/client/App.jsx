7e8604dec921b6bb1edbd03a7d3e25216a2e9f2c
josephesteva <joe.esteva@gmail.com>
2024-02-14 11:44:14 -0500

    Initial commit and single select accordion

---
+++ src/client/App.jsx
@@ -0,0 +1,13 @@
+import Accordion from "../components/accordion";
+import "./App.css";
+
+function App() {
+
+	return (
+		<div className="App">
+			<Accordion />
+		</div>
+	);
+}
+
+export default App;
