2d05f50c1a2f7ee9a9b82d64a80b8588a184cc44
josephesteva <joe.esteva@gmail.com>
2024-02-18 12:47:00 -0500

    Implement load more data component

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -3,6 +3,7 @@ import RandomColor from "../components/random-color";
 import StarRating from "../components/star-rating";
 import ImageSlider from "../components/image-slider";
 import "./App.css";
+import LoadMoreData from "../components/load-more-data";
 
 function App() {
 
@@ -11,7 +12,8 @@ function App() {
 			{/* <Accordion /> */}
 			{/* <RandomColor /> */}
 			{/* <StarRating numberOfStars={10}/> */}
-			<ImageSlider url={'https://picsum.photos/v2/list'} limit={10} />
+			{/* <ImageSlider url={'https://picsum.photos/v2/list'} limit={10} /> */}
+			<LoadMoreData />
 
 		</div>
 	);
